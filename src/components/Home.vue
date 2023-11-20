f72ba0c600f4135d9306a0ad62ba334e7a543777
Matheus Felipe Fabian <matheusffabian@gmail.com>
2023-11-20 03:03:47 -0300

    create statistics component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref } from 'vue';
+
 import ImageUploader from './ImageUploader.vue';
 import ImageTranscriber from './ImageTranscriber.vue';
+import Statistics from './Statistics.vue';
 
+let showStatistics = ref(false);
 </script>
 
 <template>
@@ -10,13 +14,22 @@ import ImageTranscriber from './ImageTranscriber.vue';
             <div class="row mt-2 flex-grow-1">
                 <div class="col-sm-12 col-md-6 d-flex">
                     <ImageUploader
-                        class="main-card flex-grow-1" />
+                        class="main-card flex-grow-1"
+                        @updateShowStatistics="showStatistics = false"
+                    />
                 </div>
 
                 <div class="col-sm-12 col-md-6 d-flex">
-                    <ImageTranscriber class="main-card flex-grow-1" />
+                    <ImageTranscriber
+                        class="main-card flex-grow-1"
+                        @updateShowStatistics="showStatistics = true"
+                    />
                 </div>
             </div>
+        </div>
+
+        <div v-if="showStatistics" class="container mt-5">
+            <Statistics :showStatistics="showStatistics" />
         </div>
     </main>
 </template>
